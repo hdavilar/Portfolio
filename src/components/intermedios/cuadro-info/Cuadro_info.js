ec9481566539f3678ef9c814723f62156b8c7d74
hdavilar <hdavilar01@gmail.com>
2026-03-25 17:02:19 +0100

    Finalizado proyecto-list y modificacion del minimo de px de los estilos

--- src/components/intermedios/cuadro-info/Cuadro_info.js
+++ src/components/intermedios/cuadro-info/Cuadro_info.js
@@ -8,7 +8,7 @@ function Cuadro_info({tema, texto1, texto2}) {
     const { t } = useTranslation();
 
     return (
-        <div className={tema === "oscuro" ? "c-info oscuro" : "c-info claro"}>
+        <div className={tema === "oscuro" ? "c-info oscuro" : "c-info"}>
             <Texto tema={tema} texto={texto1} /> {/* <Texto tema={tema} texto={t(texto1)} /> */}
             <Texto tema={tema} texto={texto2} />
         </div>
--- src/components/intermedios/proyecto-list/Proyecto_list.js
+++ src/components/intermedios/proyecto-list/Proyecto_list.js
@@ -4,19 +4,42 @@ import Texto from '../../basics/texto/Texto'
 import Titulo from '../../basics/titulo/Titulo'
 import Logo_red from '../../basics/logo_red/Logo_red'
 
-function Proyecto_list({tema}) {
+const imgsProyecto = require.context('../../../img-projects', false, /\.(png|jpe?g|webp|gif)$/)
+
+function getImagenProyecto(nombreProyecto) {
+  const nNorm = nombreProyecto.toLowerCase()
+  const extensiones = ['jpg', 'jpeg', 'png', 'webp', 'gif']
+
+  for (const extension of extensiones) {
+    const archivo = `./${nNorm}.${extension}`
+    if (imgsProyecto.keys().includes(archivo)) {
+      return imgsProyecto(archivo)
+    }
+  }
+
+  return ''
+}
+
+function Proyecto_list({tema, nombreProyecto, descripcion, enlace}) {
+  const imagenSrc = getImagenProyecto(nombreProyecto)
+
   return (
-    <li className={tema === "oscuro" ? "p-list oscuro" : "p-list claro"}>
-        <div>
+    <li className={tema === "oscuro" ? "p-list oscuro" : "p-list"}>
+        <div className='b-info'>
             {/*Imagen del proyecto*/}
-            <img src="" alt="Imagen del proyecto" />
-            {/*Nombre del proyecto*/}
-            <Titulo tema={tema} titulo="Nombre del proyecto" />
-            {/*Descripción del proyecto*/}
-            <Texto tema={tema} texto="Descripción del proyecto" />
+            <img src={imagenSrc} alt={`Imagen de ${nombreProyecto}`} />
+            <div>
+              {/*Nombre del proyecto*/}
+              <a href={`/proyectos/${nombreProyecto.toLowerCase()}`} className='enlace-p'>
+                <Titulo tema={tema} titulo={nombreProyecto} />
+              </a>
+              {/*Descripción del proyecto*/}
+              <Texto tema={tema} texto={descripcion} />
+            </div>
+            
         </div>
         {/*Enlace al proyecto (logo github)*/}
-        <Logo_red tema={tema} img="github" enlace="#" />
+        <Logo_red tema={tema} img="github" enlace={enlace} />
     </li>
   )
 }
